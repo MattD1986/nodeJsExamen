8543972c9b87b5ee88af6a8c7bf7d640f0d9397a
Matthew Desramault <matthew.desramault@student.vives.be>
2022-12-21 12:07:04 +0100

    updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if(!config.get('jwtPrivateKey')){
     process.exit(1)
   }
 
-mongoose.connect('mongodb://localhost/comicsAPI')
+mongoose.connect(config.get('database'))
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to mongodb: ', err));
 
